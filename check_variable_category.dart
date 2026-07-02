aa23d5bbc37517fa23269a107aa373c6fbdd8370
praful <prafulpatnecha@gmail.com>
2026-07-02 17:41:02 +0530

    02/07/2026 - ads setting

--- check_variable_category.dart
+++ check_variable_category.dart
@@ -1,11 +1,22 @@
 
 
 if(chiefVerificationCheck is String){
-                            if(chiefVerificationCheck=="error"){
+    if(chiefVerificationCheck=="error"){
 
-                            }
-                          }else if(chiefVerificationCheck is List){
-                            if(chiefVerificationCheck.isNotEmpty){
-                              
-                            }
-                          }
+    }
+  }else if(chiefVerificationCheck is List){
+    if(chiefVerificationCheck.isNotEmpty){
+      
+    }
+  }
+
+
+  if (snapshot.data is Map) {
+    print("Ye Map hai");
+  } else if (snapshot.data is List) {
+    print("Ye List hai");
+  } else if (snapshot.data is String) {
+    print("Ye String hai");
+  } else {
+    print("Unknown Type");
+  }
